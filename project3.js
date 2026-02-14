9ce193c50043220379a085d074c4ad9e14f02d74
aaravsha2010-cmd <aaravsha2010@gmail.com>
2026-02-14 11:09:33 -0600

    added some more features

--- project3.js
+++ project3.js
@@ -1,18 +1,35 @@
 function setup() {
   createCanvas(1300, 1300);
-  background(220)
+  background(0);
 }
 
 function draw() {
   
  noStroke();
-  fill(0, 255, 255)
-  circle(mouseX, mouseY, 75)
+  fill(0, 255, 255);
+  circle(mouseX, mouseY, 75);
   
 }
 
 function mousePressed() {
   
-  background(220)
+  background(0);
+  fill(0, 255, 255)
+  
 
+}
+
+function keyPressed() {
+  if (key === "w")
+    
+    then();
+  fill(200, 50, 30)
+  circle(mouseX, mouseY, 200)
+}
+
+
+function keyPressed() {
+  if (key === "a")
+  fill(65, 200, 120)
+  circle(mouseX, mouseY, 120)
 }
